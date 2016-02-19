067d0d0247c7a25a269db1ee823968b9791fe2ca
sabermai <szhao.sabermai@gmail.com>
2016-02-19 11:05:41 +0800

    test.java

--- SpongeCity-MonitorPortal/src/java/Test.java
+++ SpongeCity-MonitorPortal/src/java/Test.java
@@ -16,12 +16,13 @@ import java.util.List;
  */
 public class Test {
     public static void main(String[] args) {
-        String filePath = "";
+        String filePath = "/webapp/";
+        String fileName = "DeviceData.csv";
         DeviceDataFileWriter writer = new DeviceDataFileWriter();
         DataOperation dataOperation = new DataOperation();
         List<DataModel> dataModelList = new ArrayList<DataModel>();
         ModelConverter converter = new ModelConverter();
-        List<DB_DataModel> dbDataModelList = dataOperation.getData(1,3,new Date(116,0,5),new Date(116,0,9));
+        List<DB_DataModel> dbDataModelList = dataOperation.getData(1, 3, new Date(116, 0, 5), new Date(116, 0, 9));
 
         if (dbDataModelList != null) {
             AreaDataOperation areaDataOperation = new AreaDataOperation();
@@ -29,8 +30,8 @@ public class Test {
             for (DB_DataModel dbDataModel : dbDataModelList) {
                 dataModelList.add(converter.convertDBData2PortalData(dbDataModel, dbAreaModelList));
             }
-            String[] heads = new String[]{"时间","区域","地块","单项措施","设备","数据类型","值","单位"};
-            writer.writeCSV(heads,dataModelList,"","");
+            String[] heads = new String[]{"时间", "区域", "地块", "单项措施", "设备", "数据类型", "值", "单位"};
+            writer.writeCSV(heads, dataModelList, filePath, fileName);
         }
     }
 }
